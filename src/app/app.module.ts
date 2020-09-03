9fd4ded4f0f81880a930475d0c334c86d0b830c0
Gregor Woiwode <gregor.woiwode@gmail.com>
2020-09-03 10:41:33 +0200

    fix(http): fix url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/taskboard/task-data.service.ts
+++ src/app/taskboard/task-data.service.ts
@@ -1,32 +1,35 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Task } from './task';
 import { Operation } from './operation';
+import { Task } from './task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskDataService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getTasks(): Observable<Task[]>{
-    return this.http.get<Task[]>('http//:localhost:3000/tasks');
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>('http://localhost:3000/tasks');
   }
 
-  createTask(task: Task): Observable<Task>{
-    return this.http.post<Task>('http//:localhost:3000/tasks/', task);
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>('http://localhost:3000/tasks/', task);
   }
 
-  getTaskByGuid(guid: string): Observable<Task>{
-    return this.http.get<Task>(`http//:localhost:3000/tasks/${guid}`);
+  getTaskByGuid(guid: string): Observable<Task> {
+    return this.http.get<Task>(`http://localhost:3000/tasks/${guid}`);
   }
 
-  deleteTaskByGuid(guid: string): Observable<Task>{
-    return this.http.delete<Task>(`http//:localhost:3000/tasks/${guid}`);
+  deleteTaskByGuid(guid: string): Observable<Task> {
+    return this.http.delete<Task>(`http://localhost:3000/tasks/${guid}`);
   }
 
-  updateTask(guid: string, operation: Operation): Observable<Task>{
-    return this.http.put<Task>(`http//:localhost:3000/tasks/${operation}/${guid}`, null);
+  updateTask(guid: string, operation: Operation): Observable<Task> {
+    return this.http.put<Task>(
+      `http://localhost:3000/tasks/${operation}/${guid}`,
+      null
+    );
   }
 }
